371f37777af5b03ba5c2813d7c27c0ba5e956e9d
fsun71 <42247197+fsun71@users.noreply.github.com>
2019-10-01 23:32:50 -0600

    Add files via upload

--- pathPredictor.py
+++ pathPredictor.py
@@ -89,6 +89,7 @@ def processData():
 	ax = plt.axes(projection = '3d')
 
 	ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='terrain', edgecolor='none')
+	plotOptimalRoute(2437, xyzDF['elevation'].idxmax())
 	x_range = [xyzDF['longitude'].min(), xyzDF['longitude'].max()]
 	y_range = [xyzDF['lattitude'].min(), xyzDF['lattitude'].max()]
 
@@ -139,68 +140,120 @@ def nodeGraphGeneration(resolution):
 			adjacentNodeMatrix = np.sort(adjacentNodeMatrix, kind = 'mergesort')
 			adjacentNodeMatrix = np.trim_zeros(adjacentNodeMatrix)
 
-
-
 			adjacentNodeDistanceDict = {}
 
 			for i in adjacentNodeMatrix:
 				adjacentNodeElevation = nodeElevationDict[i]
 				currentNodeElevation = nodeElevationDict[boundedNodeMatrix[row][column]]
-				adjacentNodeDistanceDict.update({i : (adjacentNodeElevation - currentNodeElevation + 180)})
+				adjacentNodeDistanceDict.update({i : (adjacentNodeElevation - currentNodeElevation)})
 
 			adjacentNodeDict.update({boundedNodeMatrix[row][column] : adjacentNodeDistanceDict})
 
-	nodeGraph = adjacentNodeDict
-	return nodeGraph
+	nodeMap = adjacentNodeDict
+	return nodeMap
 
-def dijkstra(graph, srcIndex, destIndex):
+def dijkstra(nodeMap, srcIndex, destIndex):
 	inf = float('inf')
 	source = (srcIndex + 1)
 	dest =  destIndex + 1
 
-	visitedNodes = {}
-	unvisitedNodes = {}
+	#Establishes starting node and its respective distance value, establishes visited and unvisited node dictionary containers
+	visitedNodeMap = {}
+	unvisitedNodeMap = {}
+	currentDistance = 0
+	currentNode = source
 
+	#Establishes storage container for list of nodes that came before the current node, used to determine waypoints of shortest path
+	prevNodes = {}
+
+	#Distance/cost map for every node, everything gets infinite distance except the starting node, which gets a value of zero
+	#Also creates map of every node's most efficient previous iteration, allowing for paths to be known
 	nodes = np.array(range(1, resolution**2 +1))
 	for node in nodes:
 		defaultDist = inf
-		if node == source:
-			defaultDist = 0
-		unvisitedNodes.update({node : defaultDist})
+		prevNode = None
 
-	currentNode = source
-	currentDistance = 0
+		if node == source:
+			defaultDist = currentDistance
+
+		unvisitedNodeMap.update({node : defaultDist})
+		prevNodes.update({node : prevNode})
+
 
 	while True:
-	    for neighbour, distance in graph[currentNode].items():
-	        if neighbour not in unvisitedNodes: continue
-	        newDistance = currentDistance + distance
-	        if unvisitedNodes[neighbour] is None or unvisitedNodes[neighbour] > newDistance:
-	            unvisitedNodes[neighbour] = newDistance
-	    visitedNodes[currentNode] = currentDistance
-	    del unvisitedNodes[currentNode]
-	    if not unvisitedNodes: break
-	    candidates = [node for node in unvisitedNodes.items() if node[1]]
-	    currentNode, currentDistance = sorted(candidates, key = lambda x: x[1])[0]		
+	    for adjacentNode, nodeDistance in nodeMap[currentNode].items():
+	    	#Do not investigate the adjacent node if it has already been visited
+	        if adjacentNode in visitedNodeMap:
+	        	continue
+	        #Finds tentative shortest distance to currentNode from origin by summing the current distance up to the node, and the distance to the node itself
+	        tentativeDistance = currentDistance + nodeDistance
+
+	        #If the calculated tentative distance to an adjacent node is less than the currently given minimum on the unvisited node map, set the calculated tentative distance as the new minimum
+	        #Also set the most efficient previous node on the prevNodes node map
+	        if tentativeDistance < unvisitedNodeMap[adjacentNode]:
+	            unvisitedNodeMap[adjacentNode] = tentativeDistance
+	            prevNodes[adjacentNode] = currentNode
+
+	    #Once the minimum adjacent node to the current node is found, set the minimum adjacent node as the new current node and move the previous current node to the visited dictionary (never to be checked again)
+	    visitedNodeMap[currentNode] = currentDistance
+	    del unvisitedNodeMap[currentNode]
+
+
+	    #Break once minimum distance is calculated and stored (see above two lines) for the destination node
 	    if currentNode == destIndex:
 	    	break
 
-	print(visitedNodes[dest])
+	    #Set list of nodes with non-infinite distances
+	    feasibleNodes = []
+	    for node in unvisitedNodeMap.items():
+	    	if node[1] != inf:
+	    		feasibleNodes.append(node)
 
-# Used to get the 180 figure in the nodeGraphGeneration function
-# function now deprecated
-# def getMostNegative():
-# 	distMatrix = nodeGraphGeneration(resolution)
+	    #Find node in entire feasible region with lowest cost/distance value (sort dictionary by ascending value, choose key of first entry)
+	    newCurrentNode, currentDistance = sorted(feasibleNodes, key = lambda x: x[1])[0]	
 
-# 	minKeys = []
-# 	for i in range(1, 2501):
-# 		tempContainer = []
-# 		for j in distMatrix[i].values():
-# 			tempContainer.append(j)
-# 		minKeys.append(min(tempContainer))
+	    #New current node set
+	    currentNode = newCurrentNode
+
+	#Generates shortest path to the destination from the source
+	currentNode = dest
+	pathToDest = [currentNode]
+
+	while True:
+		#Stop when we get to the source
+		if prevNodes[currentNode] == None:
+			break
+		#Appends the node prior to the current one being examined to the path array
+		pathToDest.append(prevNodes[currentNode])
+		#Sets new current node as the 'previous node' in the line above, working our way back the chain
+		currentNode = prevNodes[currentNode]
+
+	return pathToDest
+
+def plotOptimalRoute(origin, destination):
+	pathToDest = dijkstra(nodeGraphGeneration(resolution), origin, destination)
+
+	lattitudePathArray = []
+	longitudePathArray = [] 
+	elevationPathArray = []
+
+	for i in pathToDest:
+		lattitudePathArray.append(xyzDF.iloc[i-1]['lattitude'])
+		longitudePathArray.append(xyzDF.iloc[i-1]['longitude'])
+		elevationPathArray.append(xyzDF.iloc[i-1]['elevation'])
+
+	X, Y, Z = lattitudePathArray, longitudePathArray, elevationPathArray
 	
-# 	mostNegative = min(np.round(minKeys, -1))
+	fig = plt.figure()
+	ax = plt.axes(projection = '3d')
 
-# 	return -1 * mostNegative
+	ax.scatter(X, Y, Z, alpha = 0)
 
-dijkstra(nodeGraphGeneration(resolution), 2437, xyzDF['elevation'].idxmax())
+	line = mplot3d.art3d.Line3D(lattitudePathArray, longitudePathArray, elevationPathArray)
+	return ax.add_line(line)
+	#return ax.scatter(X, Y, Z)
+
+processData()
+
+
+#plotOptimalRoute(2437, xyzDF['elevation'].idxmax())
