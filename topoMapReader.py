037465f28b94755923fb806543f1f3d803d2150e
Franklin Sun <42247197+fsun71@users.noreply.github.com>
2019-10-08 00:04:54 -0600

    Add files via upload

--- topoMapReader.py
+++ topoMapReader.py
@@ -1,5 +1,6 @@
 from osgeo import gdal
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import numpy as np
 import os
 
@@ -45,25 +46,43 @@ def readGeoData(mapName, NWCorner = None, SECorner = None, units = 'imperial'):
 	longitudes = [(-np.absolute(NWCornerDefault[1]) + lon*degResolution) for lon in range(divisions)]
 
 	#Finds index of closest matching subdivision to specified corner coordinates
-	regionNWLat = lattitudes.index(min(lattitudes, key = lambda x : abs(x - NWCorner[0])))
-	regionNWLong = longitudes.index(min(longitudes, key = lambda x : abs(x - NWCorner[1])))
-	regionSELat = lattitudes.index(min(lattitudes, key = lambda x : abs(x - SECorner[0])))
-	regionSELong = longitudes.index(min(longitudes, key = lambda x : abs(x - SECorner[1])))
+	regionNWLat = min(lattitudes, key = lambda x : abs(x - NWCorner[0]))
+	regionNWLong = min(longitudes, key = lambda x : abs(x - NWCorner[1]))
+	regionSELat = min(lattitudes, key = lambda x : abs(x - SECorner[0]))
+	regionSELong = min(longitudes, key = lambda x : abs(x - SECorner[1]))
+
+	NWLatIndex = lattitudes.index(regionNWLat)
+	NWLongIndex = longitudes.index(regionNWLong)
+	SELatIndex = lattitudes.index(regionSELat)
+	SELongIndex = longitudes.index(regionSELong)
 
 	#print(regionSELat, regionNWLat, regionNWLong, regionSELong)
-	regionElevData = elevData[regionSELat:regionNWLat, regionNWLong:regionSELong]
+	regionElevData = elevData[SELatIndex:NWLatIndex, NWLongIndex:SELongIndex]
+	regionBoundsArray = [regionSELat, regionNWLat, regionNWLong, regionSELong]
+	latDimension, longDimension = regionElevData.shape
 
-	return regionElevData
+	regionLatArray = np.arange(regionSELat, regionNWLat, step = (regionNWLat - regionSELat) / latDimension)
+	regionLongArray = np.arange(regionNWLong, regionSELong, step = (regionSELong - regionNWLong) / longDimension)
 
-def dataContourPlot(contourResolution = 10, title = None):
-	elevData = readGeoData('n40w107', [39.457476, -106.178689], [39.281785, -106.054310])
+	return regionElevData, regionLatArray, regionLongArray
 
-	plt.contour(elevData, cmap = "gist_earth", levels = list(range(6000, 15000, contourResolution)))
+def dataContourPlot(title = None):
+	elevData, regionLatArray, regionLongArray = readGeoData('n40w107', [39.36, -106.16], [39.3, -106.08])
+
+	scaleLowerLim = int(np.round(np.amin(elevData) * 0.95, -3))
+	scaleUpperLim = int(np.round(np.amax(elevData) * 1.05, -3))
+
+	ax = plt.axes(projection = '3d')
+	X, Y = np.meshgrid(regionLongArray, regionLatArray)
+	surfacePlot = ax.plot_surface(X, Y, elevData, cmap = "terrain", antialiased=False)
+	ax.set_xlabel('Degrees Longitude')
+	ax.set_ylabel('Degrees Lattitude')
+	ax.set_zlabel('Elevation (Feet ASL)')
 
 	if title != None:
 		plt.title("Elevation Contour Map of " + title)
 
-	cbar = plt.colorbar()
+	cbar = plt.colorbar(surfacePlot, shrink = 0.4, aspect = 10)
 
 	plt.show()
 
