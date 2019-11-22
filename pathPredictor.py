935a2bc66c715e72652fc58c991fc056e5787d3a
Franklin Sun <42247197+fsun71@users.noreply.github.com>
2019-11-22 02:15:43 -0700

    Add files via upload

--- pathPredictor.py
+++ pathPredictor.py
@@ -6,17 +6,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import topoMapReader as tMap
-from dijkstra import dijkstra
+#from dijkstraC import dijkstra
 
-resolution = 50
 name = ''
 mapName = ''
 XYCoordDict = {}
 nodeElevationDict = {}
 
 def generateNodeElevationDict():
-	NWCorner = [40.286528, -105.635828]
-	SECorner = [40.228733, -105.568827]
+	NWCorner = [40.270000, -105.645828]
+	SECorner = [40.238733, -105.608827]
 
 	#Last point in nodeElevationDict
 	NECorner = [NWCorner[0], SECorner[1]]
@@ -44,6 +43,18 @@ def generateNodeElevationDict():
 
 	for i in range(numDataPoints):
 		nodeElevationDict.update({i+1 : elevDataFlat[i]})
+
+	maxPtIndex = max(nodeElevationDict, key=nodeElevationDict.get)
+	maxPtElev = nodeElevationDict[maxPtIndex]
+
+	minPtIndex = min(nodeElevationDict, key=nodeElevationDict.get)
+	minPtElev = nodeElevationDict[minPtIndex]
+
+	global regionHighPt
+	global regionLowPt
+
+	regionHighPt = (maxPtIndex, maxPtElev)
+	regionLowPt = (minPtIndex, minPtElev)
 
 	return nodeElevationDict, numRows, numCols
 
@@ -93,87 +104,88 @@ def nodeGraphGeneration():
 			adjacentNodeDict.update({boundedNodeMatrix[row][column] : adjacentNodeDistanceDict})
 
 	nodeMap = adjacentNodeDict
+
 	return nodeMap, numRows, numCols
- 
-# def dijkstra(nodeMapOutput, srcIndex, destIndex):
-# 	inf = float('inf')
-# 	source = (srcIndex + 1)
-# 	dest =  destIndex + 1
 
-# 	nodeMap = nodeMapOutput[0]
-# 	numRows = nodeMapOutput[1]
-# 	numCols = nodeMapOutput[2]
+def dijkstra(nodeMapOutput, srcIndex, destIndex):
+	inf = float('inf')
+	source = (srcIndex + 1)
+	dest =  destIndex + 1
 
-# 	#Establishes starting node and its respective distance value, establishes visited and unvisited node dictionary containers
-# 	visitedNodeMap = {}
-# 	unvisitedNodeMap = {}
-# 	currentDistance = 0
-# 	currentNode = source
+	nodeMap = nodeMapOutput[0]
+	numRows = nodeMapOutput[1]
+	numCols = nodeMapOutput[2]
 
-# 	#Establishes storage container for list of nodes that came before the current node, used to determine waypoints of shortest path
-# 	prevNodes = {}
+	#Establishes starting node and its respective distance value, establishes visited and unvisited node dictionary containers
+	visitedNodeMap = {}
+	unvisitedNodeMap = {}
+	currentDistance = 0
+	currentNode = source
 
-# 	#Distance/cost map for every node, everything gets infinite distance except the starting node, which gets a value of zero
-# 	#Also creates map of every node's most efficient previous iteration, allowing for paths to be known
-# 	nodes = np.array(range(1, (numRows * numCols) + 1))
-# 	for node in nodes:
-# 		defaultDist = inf
-# 		prevNode = None
+	#Establishes storage container for list of nodes that came before the current node, used to determine waypoints of shortest path
+	prevNodes = {}
 
-# 		if node == source:
-# 			defaultDist = currentDistance
+	#Distance/cost map for every node, everything gets infinite distance except the starting node, which gets a value of zero
+	#Also creates map of every node's most efficient previous iteration, allowing for paths to be known
+	nodes = np.array(range(1, (numRows * numCols) + 1))
+	for node in nodes:
+		defaultDist = inf
+		prevNode = None
 
-# 		unvisitedNodeMap.update({node : defaultDist})
-# 		prevNodes.update({node : prevNode})
+		if node == source:
+			defaultDist = currentDistance
 
-# 	while True:
-# 	    for adjacentNode, nodeDistance in nodeMap[currentNode].items():
-# 	    	#Do not investigate the adjacent node if it has already been visited
-# 	        if adjacentNode in visitedNodeMap:
-# 	        	continue
-# 	        #Finds tentative shortest distance to currentNode from origin by summing the current distance up to the node, and the distance to the node itself
-# 	        tentativeDistance = currentDistance + nodeDistance
+		unvisitedNodeMap.update({node : defaultDist})
+		prevNodes.update({node : prevNode})
 
-# 	        #If the calculated tentative distance to an adjacent node is less than the currently given minimum on the unvisited node map, set the calculated tentative distance as the new minimum
-# 	        #Also set the most efficient previous node on the prevNodes node map
-# 	        if tentativeDistance < unvisitedNodeMap[adjacentNode]:
-# 	            unvisitedNodeMap[adjacentNode] = tentativeDistance
-# 	            prevNodes[adjacentNode] = currentNode
+	while True:
+	    for adjacentNode, nodeDistance in nodeMap[currentNode].items():
+	    	#Do not investigate the adjacent node if it has already been visited
+	        if adjacentNode in visitedNodeMap:
+	        	continue
+	        #Finds tentative shortest distance to currentNode from origin by summing the current distance up to the node, and the distance to the node itself
+	        tentativeDistance = currentDistance + nodeDistance
 
-# 	    #Once the minimum adjacent node to the current node is found, set the minimum adjacent node as the new current node and move the previous current node to the visited dictionary (never to be checked again)
-# 	    visitedNodeMap[currentNode] = currentDistance
-# 	    del unvisitedNodeMap[currentNode]
+	        #If the calculated tentative distance to an adjacent node is less than the currently given minimum on the unvisited node map, set the calculated tentative distance as the new minimum
+	        #Also set the most efficient previous node on the prevNodes node map
+	        if tentativeDistance < unvisitedNodeMap[adjacentNode]:
+	            unvisitedNodeMap[adjacentNode] = tentativeDistance
+	            prevNodes[adjacentNode] = currentNode
 
-# 	    #Break once minimum distance is calculated and stored (see above two lines) for the destination node
-# 	    if currentNode == destIndex:
-# 	    	break
+	    #Once the minimum adjacent node to the current node is found, set the minimum adjacent node as the new current node and move the previous current node to the visited dictionary (never to be checked again)
+	    visitedNodeMap[currentNode] = currentDistance
+	    del unvisitedNodeMap[currentNode]
 
-# 	    #Set list of nodes with non-infinite distances
-# 	    feasibleNodes = []
-# 	    for node in unvisitedNodeMap.items():
-# 	    	if node[1] != inf:
-# 	    		feasibleNodes.append(node)
+	    #Break once minimum distance is calculated and stored (see above two lines) for the destination node
+	    if currentNode == destIndex:
+	    	break
 
-# 	    #Find node in entire feasible region with lowest cost/distance value (sort dictionary by ascending value, choose key of first entry)
-# 	    newCurrentNode, currentDistance = sorted(feasibleNodes, key = lambda x: x[1])[0]	
+	    #Set list of nodes with non-infinite distances
+	    feasibleNodes = []
+	    for node in unvisitedNodeMap.items():
+	    	if node[1] != inf:
+	    		feasibleNodes.append(node)
 
-# 	    #New current node set
-# 	    currentNode = newCurrentNode
+	    #Find node in entire feasible region with lowest cost/distance value (sort dictionary by ascending value, choose key of first entry)
+	    newCurrentNode, currentDistance = sorted(feasibleNodes, key = lambda x: x[1])[0]	
 
-# 	#Generates shortest path to the destination from the source
-# 	currentNode = dest
-# 	pathToDest = [currentNode]
+	    #New current node set
+	    currentNode = newCurrentNode
 
-# 	while True:
-# 		#Stop when we get to the source
-# 		if prevNodes[currentNode] == None:
-# 			break
-# 		#Appends the node prior to the current one being examined to the path array
-# 		pathToDest.append(prevNodes[currentNode])
-# 		#Sets new current node as the 'previous node' in the line above, working our way back the chain
-# 		currentNode = prevNodes[currentNode]
+	#Generates shortest path to the destination from the source
+	currentNode = dest
+	pathToDest = [currentNode]
 
-# 	return pathToDest, numRows, numCols
+	while True:
+		#Stop when we get to the source
+		if prevNodes[currentNode] == None:
+			break
+		#Appends the node prior to the current one being examined to the path array
+		pathToDest.append(prevNodes[currentNode])
+		#Sets new current node as the 'previous node' in the line above, working our way back the chain
+		currentNode = prevNodes[currentNode]
+
+	return pathToDest, numRows, numCols
 
 #print(dijkstra(nodeGraphGeneration(), 4351, 1231))
 
@@ -200,7 +212,7 @@ def plotOptimalRoute(origin, destination):
 
 def renderVisualData3D():
 	#Fetches data regarding the optimal path coordinates, origin, destination, numRows, and numCols
-	pathXYZ, numRows, numCols = plotOptimalRoute(1, 50577)
+	pathXYZ, numRows, numCols = plotOptimalRoute(regionLowPt[0], regionHighPt[0])
 
 	#Optimal Path coordinates set
 	pathX = pathXYZ[0]
@@ -304,4 +316,5 @@ def renderVisualData2D():
 	ax.set_ylabel('Degrees lattitude')
 	plt.show()
 
+generateNodeElevationDict()
 renderVisualData3D()
